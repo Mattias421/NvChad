12c78f97b4905eafe8383187a10bc7cc915d3e01
Mattias421 <74720850+Mattias421@users.noreply.github.com>
2024-05-28 11:24:49 +0100

    Update plugins.lua

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -30,7 +30,7 @@ local plugins = {
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_syntax_enabled = 1
     vim.g.vimtex_complete_bib_simple = 1
-    vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
     vim.cmd('set spell')
     vim.cmd('set spelllang=en_gb')
     vim.cmd('inoremap <C-l> <c-g>u<Esc>[s1z=`]a<c-g>u')
